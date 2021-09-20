3348a985ef19a8f153855d4407c2bdeaa4fe8a48
longnh-uit <longnh.0601@gmail.com>
2021-09-20 11:47:04 +0700

    add dotenv

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dbLong:savemepls123@chito-stationery.mmxe4.mongodb.net/chito-stationery?retryWrites=true&w=majority', {
+    await mongoose.connect(`${process.env.MONGO_URI}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
