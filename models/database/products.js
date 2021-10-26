26acb9a428b3cb61957c2e16a9abc904c78c32e5
longnh-uit <longnh.0601@gmail.com>
2021-10-26 20:18:16 +0700

    add type to product model

--- models/database/products.js
+++ models/database/products.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({ 
     productName: { type: String, required: true },
+    type: String,
     description: String,
     detail: String,
     price: { type: mongoose.Types.Decimal128, required: true, min: 0 },
     image: { type: String, required: true },
-    type: {
+    typeList: {
         type: [
           {
             typeName: {
