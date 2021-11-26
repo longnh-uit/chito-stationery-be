efe481edfc789d8dbe102c219e2eeab754111648
Huy Võ <67512085+huyvouit@users.noreply.github.com>
2021-11-26 10:59:58 +0700

    test cmt cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 app.use('/auth', require('./routes/auth'));
 app.use('/user', require('./routes/user'));
